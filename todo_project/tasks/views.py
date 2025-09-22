ab4990eab495af1551a63218a2b58b06497d886d
Sardor <sardorbekabdurakhmono52@gmail.com>
2025-09-22 10:55:02 +0500

    Todo rpojectga yangi funksiyalar

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('index')
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('index')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('add/', views.add_task, name='add_task'),
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
